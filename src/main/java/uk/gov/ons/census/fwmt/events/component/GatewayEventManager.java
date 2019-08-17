fe32c4e3d9cdae6dab8ad79bd80b6d087b0a3c38
Simon <sdiaz1000@gmail.com>
2019-08-17 06:38:00 +0100

    moved source from parameter to a field

--- src/main/java/uk/gov/ons/census/fwmt/events/component/GatewayEventManager.java
+++ src/main/java/uk/gov/ons/census/fwmt/events/component/GatewayEventManager.java
@@ -25,6 +25,8 @@ public class GatewayEventManager {
 
   private List<String> errorEventTypes = new ArrayList<>();
 
+  private String source;
+  
   public void addEventTypes(String[] et) {
     for (String e : et) {
       eventTypes.add(e);
@@ -37,11 +39,15 @@ public class GatewayEventManager {
     }
   }
 
-  public void triggerEvent(String caseId, String source, String eventType) throws GatewayException {
-    triggerEvent(caseId, source, eventType, new HashMap<>());
+  public void setSource(String source) {
+    this.source = source;
+  }
+  
+  public void triggerEvent(String caseId, String eventType){
+    triggerEvent(caseId, eventType, new HashMap<>());
   }
 
-  public void triggerEvent(String caseId, String source, String eventType, Map<String, String> metadata) throws GatewayException {
+  public void triggerEvent(String caseId, String eventType, Map<String, String> metadata) {
     if (eventTypes.contains(eventType)) {
       GatewayEventDTO gatewayEventDTO = GatewayEventDTO.builder()
           .caseId(caseId).source(source).eventType(eventType).localTime(LocalTime.now()).metadata(metadata)
@@ -54,15 +60,15 @@ public class GatewayEventManager {
     }
   }
 
-  public void triggerErrorEvent(Class klass, Exception exception, String message, String caseId, String source, String errorEventType) throws GatewayException {
-    triggerErrorEvent(klass, exception, message, caseId, source, errorEventType, new HashMap<>());
+  public void triggerErrorEvent(Class klass, Exception exception, String message, String caseId, String errorEventType) {
+    triggerErrorEvent(klass, exception, message, caseId, errorEventType, new HashMap<>());
   }
 
-  public void triggerErrorEvent(Class klass, String message, String caseId, String source, String errorEventType) throws GatewayException {
-    triggerErrorEvent(klass, null, message, caseId, source, errorEventType, new HashMap<>());
+  public void triggerErrorEvent(Class klass, String message, String caseId, String errorEventType) {
+    triggerErrorEvent(klass, null, message, caseId, errorEventType, new HashMap<>());
   }
 
-  public void triggerErrorEvent(Class klass, Exception exception, String message, String caseId, String source, String errorEventType, Map<String, String> metadata) throws GatewayException {
+  public void triggerErrorEvent(Class klass, Exception exception, String message, String caseId, String errorEventType, Map<String, String> metadata) {
     GatewayErrorEventDTOBuilder builder = GatewayErrorEventDTO.builder()
         .className(klass.getName()).exceptionName((exception != null) ? exception.getClass().getName() : "<NONE>").message(message)
         .caseId(caseId).errorEventType(errorEventType).source(source).localTime(LocalTime.now()).metadata(metadata);
